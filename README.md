d23e1d7db42cde668f01d1eab150c71a24cfd43a
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2018-01-23 17:42:26 +0100

    ccxt to panda dataframe working
    todo : asynch method

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 [![Build Status](https://travis-ci.org/Sach97/ccxt-flask.svg?branch=master)](https://travis-ci.org/Sach97/ccxt-flask)
 
+# Introduction
+
+## Dependencies
+
+- Python 3.6.3
+- ccxt-1.10.810
+- plotly-2.2.3
+- pandas-datareader 0.5.0
+
+## Installation
+
+
--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 import ccxt
+import pandas as pd
+import time
+
+
+def poloniex_ethbtc_ohlc():
+    poloniex = ccxt.poloniex()
+    return poloniex.fetch_ohlcv('ETH/BTC')
+
+s = poloniex_ethbtc_ohlc()
+tup = tuple(map(tuple, s))
+print(pd.DataFrame(list(tup), columns=["Timestamp","Open","High","Low","Close","Volume"]))
