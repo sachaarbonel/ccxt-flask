7c79547b6978ddfd489f581ed30a963f7a8b03db
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2018-01-24 21:31:34 +0100

    cleaning of dataframe done

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import ccxt
 import pandas as pd
-import time
-
 
 def poloniex_ethbtc_ohlc():
     poloniex = ccxt.poloniex()
     return poloniex.fetch_ohlcv('ETH/BTC')
 
-s = poloniex_ethbtc_ohlc()
-tup = tuple(map(tuple, s))
-print(pd.DataFrame(list(tup), columns=["Timestamp","Open","High","Low","Close","Volume"]))
+def poloniex_historical_to_DF():
+    ohlc= poloniex_ethbtc_ohlc()
+    ohlc_tup = tuple(map(tuple, ohlc))
+    historical_DF = pd.DataFrame(list(ohlc_tup), columns=["Date","Open","High","Low","Close","Volume"])
+    historical_DF["Date"] = historical_DF["Date"].apply(pd.to_datetime,unit='ms')
+    historical_DF.index= historical_DF["Date"].apply(pd.to_datetime,format='%Y-%m-%d %H:%M:%S.%f')
+    clean_DF = historical_DF.drop('Date', axis=1)
+    return clean_DF
